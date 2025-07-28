da3241e0b3cb41f4176698b3cf711aa4985f510c
OuelhaziArij <welhaziarij7@gmail.com>
2025-07-28 13:30:42 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "calc-cache-v1";
+const urlsToCache = ["/", "/index.html", "/manifest.json", "/calcul.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
